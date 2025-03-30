dc55f87774b7277e31527b09cf22f69bbaa7f3f2
Drew Haven <drew.haven@gmail.com>
2025-03-30 11:25:11 -0700

    Removes Thumbstick config to reuse the mapping for an older rev1.2

---
+++ keyboards/splitkb/kyria/keymaps/periodic_thumbstick/keycodes.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+enum layers {
+    _COLEMAK = 0,
+    _QWERTY,
+    _MAC,
+    _ARPG,
+    _FPS,
+    _FPS2,
+    _MMORPG,
+    _MHR,
+    _SYMB,
+    _MOVE,
+    _UTIL,
+    _WIN,
+};
+
